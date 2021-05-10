74ffe7046b94250e069b9f44387329dc0fa6263f
Jesse Padilla <jpadillaa@gmail.com>
2021-05-10 17:07:45 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+salario = float (input("Ingrese su salario mensual: "))
+
+pago_salud = (salario * 0.4) * 0.125
+pago_pension = (salario * 0.4) * 0.16
+pago_riesgos =  (salario * 0.4) * 0.00522
+
+print("\n\nPago al sistema de salud = ", pago_salud)
+print("Pago al sistema de pension = ", pago_pension)
+print("Pago al sistema de riesgos = ", pago_riesgos)
+
+print("Salario neto = ", salario - pago_salud - pago_pension - pago_riesgos, "\n\n")
