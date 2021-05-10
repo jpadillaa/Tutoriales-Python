7314f8d52eae218f6abf78027b4ad27623c930c2
Jesse Padilla <jpadillaa@gmail.com>
2021-05-10 17:09:14 -0500

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,8 +4,8 @@ pago_salud = (salario * 0.4) * 0.125
 pago_pension = (salario * 0.4) * 0.16
 pago_riesgos =  (salario * 0.4) * 0.00522
 
-print("\n\nPago al sistema de salud = ", pago_salud)
+print("Pago al sistema de salud = ", pago_salud)
 print("Pago al sistema de pension = ", pago_pension)
 print("Pago al sistema de riesgos = ", pago_riesgos)
 
-print("Salario neto = ", salario - pago_salud - pago_pension - pago_riesgos, "\n\n")
+print("Salario neto = ", salario - pago_salud - pago_pension - pago_riesgos)
